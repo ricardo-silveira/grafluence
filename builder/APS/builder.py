a99d91e017101bdaa5810ebd5a6f9a1f08d50e48
ricardo-silveira <ricardosilveira@poli.ufrj.br>
2016-05-28 19:26:26 -0300

    Extracting authors and publication date from files

--- builder/APS/builder.py
+++ builder/APS/builder.py
@@ -21,7 +21,20 @@ class Builder(object):
 
     def build_path(self, vertex_label, dir_path=self.COAUTHORSHIP_DIR_PATH):
         """
-        Creates path to vertex file
+        Creates path to metadata-json file
+
+        Parameters
+        ----------
+        vertex_label: str
+            label for vertex/work in csv file
+        dir_path: str
+            root path to be built on
+
+        Returns
+        -------
+        str
+            Path for json file which contains information about work,
+            such as the authors and publication date.
         """
         file_name = vertex_label.split("/")[1]
         vertex_path = "%s/" % dir_path
@@ -34,20 +47,49 @@ class Builder(object):
         vertex_path = "%s/%s.json" % (vertex_path, file_name)
         return vertex_path
 
-    def get_vertices_path(self):
+    def get_works_path(self):
         """
-        Returns dictionary of vertices and their respective json file,which
-        contains information about each paper
+        Returns dictionary of vertices and their respective json file path,
+        which contains information about each work
+
+        Returns
+        -------
+        dict
+            Mapping work name and its corresponding json file path.
         """
-        vertices = {}
+        works = {}
         with open(self.CITATION_CSV_PATH) as citation_edges:
             first_line = True
             for edges in citation_edges:
                 if not first_line:
                     target, source = edges.rstrip("\n").split(",")
-                    if source not in vertices:
-                        vertices[source] = self.build_path(source)
-                    if target not in vertices:
-                        vertices[target] = self.build_path(target)
+                    if source not in works:
+                        works[source] = self.build_path(source)
+                    if target not in works:
+                        works[target] = self.build_path(target)
                 first_line = False
-        return vertices
+        return works
+
+    def get_work_info(self, work_path):
+        """
+        Returns relevant information from work.
+
+        Parameters
+        ---------
+        work_path: str
+            Path for json file which contains all information
+
+        Returns
+        -------
+        dict
+            Keys "authors", holding a list of authors names, and
+            "publication_date".
+        """
+        authors = []
+        publication_date = None
+        work_data = json.loads(open(work_path))
+        publication_date = work_data["date"]
+        for author in work_date["authors"]:
+            authors.append(author["name"])
+        return {"authors": authors,
+                "publication_date": publication_date}

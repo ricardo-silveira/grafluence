2d16144a8f08809677102a9233764b0fb9830609
ricardo-silveira <ricardosilveira@poli.ufrj.br>
2016-06-19 01:34:21 -0300

    Adjusting tools package

---
+++ tools/dynamic.py
@@ -0,0 +1,14 @@
+"""
+Analyzes graph dynamics
+"""
+
+
+class Dynamic(object):
+    """
+    """
+    def __init__(self):
+        self._g1 = None
+        self._g2 = None
+
+    def get_edges(self):
+        pass

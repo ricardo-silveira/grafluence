b389803cca56ab98086c1b2c35f10024d4271bcf
ricardo-silveira <ricardosilveira@poli.ufrj.br>
2016-08-23 22:30:31 -0300

    parent class adjusments

--- builder/__init__.py
+++ builder/__init__.py
@@ -1,1 +1,2 @@
 from builder import Buider
+import _helper
--- builder/builder.py
+++ builder/builder.py
@@ -2,6 +2,8 @@
 """
 import os
 import csv
+from subprocess import call
+
 
 class Builder(object):
     """
@@ -11,7 +13,8 @@ class Builder(object):
         Exports graph file in csv format
         """
         header = kwargs.get("header", None)
-        with open(output_path, "wb") as graph_file:
+        open_mode = kwargs.get("open_mode", "wb")
+        with open(output_path, open_mode) as graph_file:
             writer = csv.writer(graph_file)
             if header:
                 writer.writerow(header)
@@ -31,9 +34,34 @@ class Builder(object):
             graph[v_i][v_j] = 0.0
         graph[v_i][v_j] += weight
 
-    def standard_dirs(self):
+    def unify_edges(graph_path):
         """
         """
-        export_path = kwargs.get("export_path", "data/")
-        if not os.path.exists(export_path):
-            os.makedirs(export_path)
+        temp_graph = "%.temp" % graph_path
+        # sorting graph file
+        call('(head -n 1 %(file)s && tail -n +2 %(file)s | sort) > %(file)s' % {"file": graph_path})
+        with open(temp_graph, "wb") as temp_graph:
+            temp_writer = csv.writer(temp_graph, delimiter=",")
+            with open(graph_path, "r") as graph:
+                first_line = True
+                second_line = True
+                graph_reader = csv.reader(graph, delimiter=",")
+                for row in graph_reader:
+                    if first_line:
+                        temp_writer.writerow(row)
+                        first_line = False
+                    # Saving first edge and its weight
+                    if second_line:
+                        v_i, v_j, e_w = row
+                        second_line = False
+                    # Check in rest of file if the same edge happens
+                    else:
+                        n_v_i, n_v_j, n_e_w = row
+                        # if same edge, add its weight
+                        if n_v_1 == v_i and n_v_j == v_j:
+                            e_w += n_e_w
+                        # When finding new edge, write previous on file
+                        else:
+                            temp_writer.writerow([v_i, v_j, n_e_w])
+                            v_i, v_j, e_w = n_v_i, n_v_j, n_e_w
+        call("mv %s %s" % (temp_graph, graph_path))
